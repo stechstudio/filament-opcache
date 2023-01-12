e0b99ef3728efb01eec3ecd3a953c13d2e7ad392
DP <dp@reproconnect.com>
2023-01-12 11:07:34 -0500

    filament PluginServiceProvider

--- src/LaravelFilamentOpcacheServiceProvider.php
+++ src/LaravelFilamentOpcacheServiceProvider.php
@@ -1,6 +1,12 @@
 namespace STS\LaravelFilamentOpcache;
 
-class LaravelFilamentOpcacheServiceProvider extends ServiceProvider
+use Filament\PluginServiceProvider;
+use Spatie\LaravelPackageTools\Package;
+
+class LaravelFilamentOpcacheServiceProvider extends PluginServiceProvider
 {
-    
+    public function configurePackage(Package $package): void
+    {
+        $package->name('laravel-filament-opcache');
+    }
 }
