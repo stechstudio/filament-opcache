391552517eb4053ccef823d9fc314ed3e8305da4
DP <dp@reproconnect.com>
2023-01-12 11:26:08 -0500

    opcache page

--- src/LaravelFilamentOpcacheServiceProvider.php
+++ src/LaravelFilamentOpcacheServiceProvider.php
@@ -4,9 +4,14 @@ namespace STS\LaravelFilamentOpcache;
 
 use Filament\PluginServiceProvider;
 use Spatie\LaravelPackageTools\Package;
+use STS\LaravelFilamentOpcache\Pages\Opcache;
 
 class LaravelFilamentOpcacheServiceProvider extends PluginServiceProvider
 {
+    protected array $pages = [
+        Opcache::class,
+    ];
+
     public function configurePackage(Package $package): void
     {
         $package->name('laravel-filament-opcache');
---
+++ src/Pages/Opcache.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace STS\LaravelFilamentOpcache\Pages;
+
+use Appstract\Opcache\OpcacheFacade;
+use Filament\Notifications\Notification;
+use Filament\Pages\Actions\Action;
+use Filament\Pages\Page;
+
+class Opcache extends Page
+{
+    protected static ?string $title = 'OPcache';
+
+    protected static ?string $navigationLabel = 'OPcache';
+
+    protected static ?string $slug = 'opcache';
+
+    protected static ?string $navigationIcon = 'heroicon-o-database';
+
+    protected static string $view = 'filament.pages.opcache';
+
+    protected static ?string $navigationGroup = 'System Management';
+
+    protected function getActions(): array
+    {
+        return [
+            Action::make('compile')
+                ->label('Compile Scripts')
+                ->action(function () {
+                    $result = OpcacheFacade::compile(true);
+
+                    Notification::make()
+                        ->title('Scripts Compiled')
+                        ->body($result['compiled_count'] . ' files compiled successfully.')
+                        ->success()
+                        ->send();
+                }),
+            Action::make('clear')
+                ->label('Clear OPcache')
+                ->action(function () {
+                    if (OpcacheFacade::clear()) {
+                        Notification::make()->title('OPcache Cleared')->success()->send();
+                    } else {
+                        Notification::make()->title('OPcache Disabled')->warning()->send();
+                    }
+                }),
+        ];
+    }
+}
