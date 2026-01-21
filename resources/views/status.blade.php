2e0887a961ffee2444507c87342140faad45164a
Haz <hello@haztakki.solutions>
2026-01-21 11:43:58 +0200

    Remove the card

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -14,25 +14,23 @@
         </div>
 
         <div wire:poll.10s>
-            <x-filament::card>
-                <table class="min-w-full divide-y divide-gray-200">
-                    <thead class="bg-gray-50">
-                        <tr>
-                            <th class="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Key</th>
-                            <th class="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Value</th>
-                        </tr>
-                    </thead>
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th class="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Key</th>
+                        <th class="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Value</th>
+                    </tr>
+                </thead>
 
-                    <tbody class="bg-white divide-y divide-gray-200">
-                        @foreach($$activeTab as $key => $value)
-                            <tr>
-                                <td class="px-4 py-1">{{ $key }}</td>
-                                <td class="px-4 py-1">{{ $value }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </x-filament::card>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach($$activeTab as $key => $value)
+                        <tr>
+                            <td class="px-4 py-1">{{ $key }}</td>
+                            <td class="px-4 py-1">{{ $value }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </x-filament::page>
